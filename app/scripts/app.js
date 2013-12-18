ed5144bac3986e99c2f13cbb5f81f23bc3a8a412
Viacheslav Ivanov <slava.eth@gmail.com>
2013-12-17 22:21:43 -0800

    added check for user authentication

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,43 +9,73 @@ var pinapleApp = angular.module('pinapleApp', [
 
   .config(['$stateProvider', '$urlRouterProvider',
     function ($stateProvider, $urlRouterProvider) {
-    
-    urlRouterProvider.otherwise( '/dashboard' );
-    
-    // setup states
-    $stateProvider
-      // user management
-      .state('login', {
-        url: '/login',
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
-      })
-      .state('logout', {
-        url: '/logout',
-        controller: 'LogoutCtrl',
-        resolve: { loggedin: isLoggedIn }
-      })
-      .state('signup', {
-        url: '/signup',
-        templateUrl: 'views/signup.html',
-        controller: 'SignupCtrl'
-      })
-      .state('forgot', {
-        url: '/forgot',
-        templateUrl: 'views/forgot.html',
-        controller: 'ForgotCtrl'
-      })
 
-      .state('main', {
-        abstract: true,
-        templateUrl: 'views/main.html'
-      })
+      // checks if the user is authenticated
+      var isLoggedIn = function($q, $http, $location, Auth){
+        var deferred = $q.defer();
 
-      .state('main.dashboard', {
-        url: '/dashboard',
-        templateUrl: 'views/dashboard.html',
-        controller: 'DashboardCtrl',
-        resolve: { loggedin: isLoggedIn }
-      })
+        $http.get( '/loggedin' ).success(
+          function (response){
+            if( response !== '0' ) {
+              // if authenticated on server then
+              if( !Auth.loggedIn ) {
+                // if not authenticated in app then
+                Auth.login( response.sid, response.auth_token ).then(
+                  function (user) {
+                    deferred.resolve( user );
+                  },
+                  function (response) {
+                    
+                  });
+              }
+              deferred.resolve( Auth.user );
+            }
+            else {
+              // not authenticated on server
+              deferred.reject();
+              $location.url( 'login' );
+            }
+          });
+
+        return deferred.promise;
+      };
+    
+      $urlRouterProvider.otherwise( '/dashboard' );
+      
+      // setup states
+      $stateProvider
+        // user management
+        .state('login', {
+          url: '/login',
+          templateUrl: 'views/login.html',
+          controller: 'LoginCtrl'
+        })
+        .state('logout', {
+          url: '/logout',
+          controller: 'LogoutCtrl',
+          resolve: { loggedin: isLoggedIn }
+        })
+        .state('signup', {
+          url: '/signup',
+          templateUrl: 'views/signup.html',
+          controller: 'SignupCtrl'
+        })
+        .state('forgot', {
+          url: '/forgot',
+          templateUrl: 'views/forgot.html',
+          controller: 'ForgotCtrl'
+        })
+
+        .state('main', {
+          abstract: true,
+          templateUrl: 'views/main.html'
+        })
+
+        .state('main.dashboard', {
+          url: '/dashboard',
+          templateUrl: 'views/dashboard.html',
+          controller: 'DashboardCtrl',
+          resolve: { loggedin: isLoggedIn }
+        })
 
   }]);
