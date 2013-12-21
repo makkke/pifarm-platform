91ba43855dd1b0ec1d4d01c1d6186d9a7d0ea51d
Viacheslav Ivanov <slava.eth@gmail.com>
2013-12-21 01:26:36 -0800

    added comments

--- app/scripts/controllers/SignupCtrl.js
+++ app/scripts/controllers/SignupCtrl.js
@@ -3,19 +3,25 @@
 pinapleApp
   .controller('SignupCtrl', ['$scope', 'DataSvc', function ($scope, DataSvc) {
 
+    /*
+     * Creates a new user and logs him in
+     * @param object User
+     * @param object Form
+     * @return bool New spinner status
+     */
     $scope.signup = function (user, form) {
       if( form.$valid ) {
         $scope.startSpinner();
         
         if( !$scope.checkPasswordLength( user.password ) ) {
           $scope.stopSpinner();
-          $scope.error = 'min';
+          $scope.showError( 'min' );
           return;
         }
 
         if( !$scope.checkPasswordsMatch( user.password, user.passwordConfirmation ) ) {
           $scope.stopSpinner();
-          $scope.error = 'match';
+          $scope.showError( 'match' );
           return;
         }
 
@@ -23,25 +29,52 @@ pinapleApp
       }
     }
 
+    /*
+     * Starts a loading spinner
+     * @return bool New spinner status
+     */
     $scope.startSpinner = function () {
-      $scope.loading = true;
+      return $scope.loading = true;
     };
 
+    /*
+     * Stops a loading spinner
+     * @return bool New spinner status
+     */
     $scope.stopSpinner = function () {
-      $scope.loading = false;
+      return $scope.loading = false;
     };
- 
+
+    /*
+     * Shows an error near input
+     * @param string Error code can be 'used', 'match', 'min'
+     * @return string New error code
+     */
+    $scope.showError = function (errorCode) {
+      return $scope.error = errorCode;
+    };
+  
+    /*
+     * Checks if password is longer than minimum length
+     * @param string Password
+     * @return bool
+     */
     $scope.checkPasswordLength = function (password) {
       var minPasswordLength = 6;
       return password.length >= minPasswordLength;
     };
 
+    /*
+     * Checks if password matches password confirmation.
+     * @param string Password
+     * @param string Password confirmation
+     * @return bool
+     */
     $scope.checkPasswordsMatch = function (password, confirmation) {
       return password === confirmation;
     };
     
     $scope.loading = false;
-    $scope.error = '';     // error code can be used, match, min
 
     $scope.descriptionTypes = DataSvc.descriptionTypes;
 
