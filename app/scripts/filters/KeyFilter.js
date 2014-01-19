0df2714e28b5cbb40872a9dbc15e2e4ff4edc10e
Viacheslav Ivanov <slava.eth@gmail.com>
2014-01-18 18:20:47 -0800

    added key filter

---
+++ app/scripts/filters/KeyFilter.js
@@ -0,0 +1,16 @@
+'use strict';
+
+pinapleApp
+  .filter('keyFilter', [function () {
+    
+    return function (key) {
+      var MAX_LENGTH = 8;
+
+      if( angular.isUndefined( key ) ) return;
+
+      if( key.length <= MAX_LENGTH ) return key;
+
+      return key.substring( 0, MAX_LENGTH ) + '...';
+    }
+
+  }]);
