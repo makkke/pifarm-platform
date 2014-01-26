09f830dd03d0b042f50ea7f28da8425829e48b2e
Viacheslav Ivanov <slava.eth@gmail.com>
2014-01-25 22:57:27 -0800

    added pinaples repo

--- app/scripts/controllers/FarmCtrl.js
+++ app/scripts/controllers/FarmCtrl.js
@@ -1,9 +1,18 @@
 'use strict';
 
 pinapleApp
-  .controller('FarmCtrl', ['$scope', '$window', function ($scope, $window) {
+  .controller('FarmCtrl', ['$scope', '$window', 'PinaplesRepoSvc',
+    function ($scope, $window, PinaplesRepoSvc) {
     
     $scope.title = 'My Farm | Pinaple';
     $window.document.title = $scope.title;
+
+    PinaplesRepoSvc.query().then(
+      function (pinaples) {
+        $scope.pinaples = pinaples;
+      },
+      function (error, status) {
+        console.log( 'error:', error );
+      });
 
   }]);
