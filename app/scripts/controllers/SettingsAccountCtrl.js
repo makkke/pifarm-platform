11a46434f29c1c33f899106700297cb45a49e803
Viacheslav Ivanov <slava.eth@gmail.com>
2014-02-22 22:53:16 -0800

    partially added account reset password

--- app/scripts/controllers/SettingsAccountCtrl.js
+++ app/scripts/controllers/SettingsAccountCtrl.js
@@ -1,11 +1,30 @@
 'use strict';
 
 angular.module('pifarmApp')
-  .controller('SettingsAccountCtrl',
-  ['$scope', '$window',
-  function ($scope, $window) {
+.controller('SettingsAccountCtrl',
+['$scope', 'AccountValidatorSvc',
+function ($scope, AccountValidator) {
 
-    $scope.title = 'Account Settings | Pinaple Farm';
-    $window.document.title = $scope.title;
-    
-  }]);
+  $scope.loading = false;
+  $scope.error = '';
+
+  $scope.update = function (form, password) {
+    if( form.$valid ) {
+      $scope.laoding = true;
+      $scope.error = '';
+
+      if( ! AccountValidator.check_password_length(password.old) ) {
+        $scope.loading = false;
+        $scope.show_error( 'incorrect' );
+        return;
+      }
+
+      if( ! AccountValidator.check_password_length(password.new) ) {
+        $scope.loading = false;
+        $scope.show_error( 'min' );
+        return;
+      }
+    }
+  }; 
+  
+}]);
