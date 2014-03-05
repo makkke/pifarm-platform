48650c688eddfa997826e40d10bf404d07c750a2
Viacheslav Ivanov <slava.eth@gmail.com>
2014-03-04 19:25:58 -0800

    fixed issue with signup not showing server error

--- app/scripts/controllers/SignupCtrl.js
+++ app/scripts/controllers/SignupCtrl.js
@@ -1,59 +1,57 @@
 'use strict';
 
 angular.module('pifarmApp')
-  .controller('SignupCtrl',
-  ['$scope', '$window', '$location', 'AuthSvc', 'ApiErrorSvc', 'AccountValidatorSvc',
-  function ($scope, $window, $location, AuthSvc, ApiErrorSvc, AccountValidator) {
+.controller('SignupCtrl',
+['$scope', '$window', '$location', 'AuthSvc', 'ApiErrorSvc', 'AccountValidatorSvc',
+function ($scope, $window, $location, AuthSvc, ApiErrorSvc, AccountValidator) {
 
-    $scope.title = 'Sign Up | Pinaple Farm';
-    $window.document.title = $scope.title;
-    
-    $scope.loading = false;
-    $scope.error = '';
+  $scope.title = 'Sign Up | Pinaple Farm';
+  $window.document.title = $scope.title;
+  
+  $scope.loading = false;
+  $scope.error = '';
 
-    /*
-     * Create new account and redirect to login page
-     * @param object account
-     * @param object form
-     */
-    $scope.signup = function (account, form) {
-      if( form.$valid ) {
-        $scope.loading = true;
-        $scope.error = '';
-        
-        if( ! AccountValidator.check_password_length(account.password) ) {
-          $scope.loading = false;
-          $scope.show_error( 'min' );
-          return;
-        }
-
-        AuthSvc.signup( account ).then(
-          function (account) {
-            $scope.loading = false;
-            $location.url( 'login?username=' + account.username );
-          },
-          function (error) {
-            $scope.loading = false;
-            
-            if( ApiErrorSvc.server_error( status ) ) {
-              $scope.show_error( 'server' );
-              return;
-            }
-            
-            if( _.contains(error.errors, 'The email has already been taken.' ) ) {
-              $scope.show_error( 'used' );
-            }
-          });
+  /*
+   * Create new account and redirect to login page
+   * @param object account
+   * @param object form
+   */
+  $scope.signup = function (account, form) {
+    if( form.$valid ) {
+      $scope.loading = true;
+      $scope.error = '';
+      
+      if( ! AccountValidator.check_password_length(account.password) ) {
+        $scope.loading = false;
+        $scope.show_error( 'min' );
+        return;
       }
+
+      AuthSvc.signup( account ).then(
+      function (account) {
+        $scope.loading = false;
+        $location.url( 'login?username=' + account.username );
+      },
+      function (error) {
+        $scope.loading = false;
+        
+        if( _.contains(error.errors, 'The email has already been taken.' ) ) {
+          $scope.show_error( 'used' );
+        }
+        else {
+          $scope.show_error( 'server' );
+        }
+      });
     }
+  }
 
-    /*
-     * Shows an error near input
-     * @param string Error code, possible values 'used' || 'min' || 'server'
-     * @return string New error code
-     */
-    $scope.show_error = function (error_code) {
-      return $scope.error = error_code;
-    };
+  /*
+   * Shows an error near input
+   * @param string Error code, possible values 'used' || 'min' || 'server'
+   * @return string New error code
+   */
+  $scope.show_error = function (error_code) {
+    return $scope.error = error_code;
+  };
 
-  }]);
+}]);
