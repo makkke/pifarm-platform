81b0d031ea0e2b013197bf8e293f33a5b382f817
Viacheslav Ivanov <slava.eth@gmail.com>
2014-03-05 23:59:46 -0800

    added 'delete slice' feature

--- app/scripts/controllers/SliceGeneral.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('pifarmApp')
-.controller('SliceGeneralCtrl',
-['$scope', '$location', 'DevicesRepoSvc', 'DeviceValidatorSvc', 'DataSvc', 'ApiErrorSvc', 'device',
-function ($scope, $location, DevicesRepo, DeviceValidator, Data, ApiErrorSvc, device) {
-  
-
-}]);
